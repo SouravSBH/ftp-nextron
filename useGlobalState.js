e129910a9eb182794ae66f1f4c84ac3d2e896e7c
SouravSBH <124552082+SouravSBH@users.noreply.github.com>
2024-02-22 14:29:55 +0600

    Update useGlobalState.js

--- useGlobalState.js
+++ useGlobalState.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 let globalState = {};
 
 let listeners = {};
 
 export const useGlobalState = (scope = "GLOBAL", initState) => {
-  const setState = useState(
+  const [state, setState] = useState(
     globalState[scope]
       ? { ...globalState[scope], ...initState }
       : (globalState[scope] = initState)
-  )[1];
+  );
 
   const setStates = (newState) => {
     if (typeof newState === "function") {
@@ -22,6 +22,21 @@ export const useGlobalState = (scope = "GLOBAL", initState) => {
     }
   };
 
+  const dispatch = useCallback(
+    async (action, payload) => {
+      if (!action || !(action instanceof Function)) {
+        return;
+      }
+      const newState = await action(payload, state, dispatch, setStates);
+      if (newState !== undefined) {
+        setStates(newState);
+      }
+    },
+    [state]
+  );
+
+  console.log();
+
   useEffect(() => {
     listeners[scope] = listeners[scope]
       ? [...listeners[scope], setState]
@@ -31,5 +46,69 @@ export const useGlobalState = (scope = "GLOBAL", initState) => {
     };
   }, [setState]);
 
-  return [globalState[scope], setStates];
+  return [globalState[scope], setStates, dispatch];
+};
+
+
+import {
+  deleteConversationMessage,
+  getStylistConversationSearchList,
+} from "api-routes/ApiRoutes";
+import { resStatus } from "lib/constants/enum/ResponseType";
+
+export const messageActions = {
+  START_LOADING: () => {
+    return (prev) => ({ ...prev, status: resStatus.LOADING });
+  },
+  DELETE_MESSAGE: async (
+    { message, conversationId, everyone },
+    _,
+    dispatch
+  ) => {
+    try {
+      dispatch(messageActions.START_LOADING);
+      await deleteConversationMessage({
+        conversationId,
+        messageIds: message._id,
+        everyone: everyone,
+      });
+      return {
+        status: resStatus.SUCCESS,
+      };
+    } catch (e) {
+      return {
+        status: resStatus.ERROR,
+      };
+    }
+  },
+  SEARCH_MESSAGE: async ({ conversationId, searchQuery }, state, setState) => {
+    console.log(setState);
+    try {
+      setState((state) => {
+        return {
+          ...state,
+          status: resStatus.LOADING,
+        };
+      });
+      const res = await getStylistConversationSearchList(
+        conversationId,
+        searchQuery
+      );
+      if (!res || res.data.type == resStatus.ERROR) {
+        return {
+          status: resStatus.ERROR,
+          data: null,
+        };
+      }
+      return {
+        status: resStatus.SUCCESS,
+        data: res?.data,
+      };
+    } catch (e) {
+      return {
+        status: resStatus.ERROR,
+        data: null,
+      };
+    }
+  },
 };
